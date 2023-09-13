3669c8c7830fc6baaead26505dc06a252fe7ad12
hectorcymbiotika <hector@cymbiotika.com>
2023-09-13 07:52:52 -0700

    fix to prevent break for mySavedPosts is undefined

--- src/social/components/SavedPosts/index.js
+++ src/social/components/SavedPosts/index.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import Post from '~/social/components/post/Post';
 import DefaultPostRenderer from '~/social/components/post/Post/DefaultPostRenderer';
 import { SavedPostsContainer } from './styles';
+import EmptyState from '../EmptyState';
+import { EmptyStateContainer } from '../EmptyState/styles';
 
 import ServerAPI from '~/social/pages/Application/ServerAPI';
 
@@ -31,7 +33,18 @@ const SavedPosts = ({ userId, isMe }) => {
   return (
     <SavedPostsContainer>
       {loading && renderLoadingSkeleton()}
-      {!loading && mySavedPosts.map((postId, index) => <Post key={index} postId={postId} />)}
+      {!loading && mySavedPosts === undefined ? (
+        <EmptyStateContainer>
+          <EmptyState
+            // icon={icon}
+            title="Nothing to see here."
+            description="You have no saved posts"
+          />
+        </EmptyStateContainer>
+      ) : null}
+      {!loading &&
+        mySavedPosts &&
+        mySavedPosts.map((postId, index) => <Post key={index} postId={postId} />)}
     </SavedPostsContainer>
   );
 };
